7d2bef9adbe3910e5dd228493363648306a83489
Pratyusha <44306139+gudiausha@users.noreply.github.com>
2020-04-29 01:35:18 +0530

    Update views.py

--- website/website_main/datalist/views.py
+++ website/website_main/datalist/views.py
@@ -6,7 +6,7 @@ from flask_mail import Message, Mail
 from website_main import mail
 
 datalist = Blueprint('datalist',__name__)
-
+# lists all the posts within recipe category
 @datalist.route('/relist')
 def recipe_list():
     re = blogposts.query.filter_by(category='recipes').all()
@@ -16,6 +16,7 @@ def recipe_list():
     else:
         return render_template("404.html")
 
+# lists all posts within the books category    
 @datalist.route('/bolist')
 def books_list():
     bo = blogposts.query.filter_by(category='books').all()
@@ -25,6 +26,7 @@ def books_list():
     else:
         return render_template("404.html")
 
+# lists all posts within the thoughts category
 @datalist.route('/thlist')
 def thoughts_list():
     th = blogposts.query.filter_by(category='thoughts').all()
@@ -34,12 +36,14 @@ def thoughts_list():
     else:
         return render_template("404.html")
 
+# opens up individual post(based on post id) irrespective of the category
 @datalist.route('/<int:id>')
 def article(id):
     article = blogposts.query.get_or_404(id)
     return render_template('article.html',title=article.title,
                             date=article.date,post=article,content=article.content)
 
+# contact form:opens up when reader wants to send a msg to the author
 @datalist.route('/contact',methods=['GET', 'POST'])
 def contact():
     form = contactform()
@@ -49,7 +53,7 @@ def contact():
             flash('All fields are required.')
             return render_template('contact.html', form=form)
         else:
-            msg = Message(form.subject.data, sender='gudiawebsite@gmail.com', recipients=['gudiawebsite@gmail.com'])
+            msg = Message(form.subject.data, sender='sender@example.com', recipients=['recipient@example.com'])
             msg.body = """
                         From: %s <%s>
                         %s
