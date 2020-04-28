f94acae69a5599f53c0b6aa3d910aae476a33962
Pratyusha <44306139+gudiausha@users.noreply.github.com>
2020-04-29 02:40:58 +0530

    Update models.py

--- website/website_main/models.py
+++ website/website_main/models.py
@@ -1,5 +1,4 @@
-# don't know if it will be needed for me
-# this is to hold my 3 tables:books,recipes,randoms
+# holds the table called blogposts which stores the data
 from datetime import datetime
 from website_main import db
 
@@ -7,15 +6,13 @@ class blogposts(db.Model):
     __tablename__ = 'blogposts'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(1000))
-    # pics = db.Column(db.String(100))
     content = db.Column(db.Text)
     date = db.Column(db.DateTime,nullable=False,default=datetime.utcnow)
     category = db.Column(db.String(100))
-    # will be adding image as well
+    
 
     def __init__(self, title,content,category):
         self.title = title
-        # self.pics = pics
         self.content = content
         self.category = category
 
@@ -26,8 +23,3 @@ class blogposts(db.Model):
 
 
 
-# class books():
-#     pass
-#
-# class randoms():
-#     pass
